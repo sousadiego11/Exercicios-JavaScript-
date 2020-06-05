b82d30cdaa6fccf70c6ec7b8cdfbc9523792a25a
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-05 19:15:17 -0300

    Add files via upload

---
+++ Exercicio array001.js
@@ -0,0 +1,8 @@
+
+const valores = ["James", 1.1, 6.1, 9, false];
+var nome = valores[3];
+let boolean = 5<2 || 5>2;
+
+console.log(valores);
+console.log(`${valores[2]}, ${valores[0]}, ${boolean}`);
+console.log(`Olá ${valores[0]}! você possui ${valores.length} valores > ${boolean}`)
